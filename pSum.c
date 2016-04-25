dcaec23ca956ad29923c113fb6016a7d0bcf60b8
Kevin Aud <kevinaud@gmail.com>
2016-04-24 22:21:13 -0500

    added src files

---
+++ pSum.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <sys/wait.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#include "functions.h"
+
+int processing_function(int fhandle){
+	int sum;
+	sum = 0;
+
+	int buffer[1];
+	while(read(fhandle, buffer, sizeof(int)) != 0){
+		flock(fhandle, LOCK_EX);
+
+		sum += buffer[0];
+
+		flock(fhandle, LOCK_UN);
+	}	
+
+	return sum;	
+}
+
+int main(int argc, char* argv[]){
+	
+	if(argc != 3){
+		exit(1);
+	}
+
+	char* 	filename; 
+	char* 	numProcessStr;
+	int		numProcesses;
+
+	filename = stringCopy(argv[1]);
+
+	numProcessStr = stringCopy(argv[2]);
+	numProcesses = atoi(numProcessStr);
+	free(numProcessStr);
+
+	int	fhandle;
+
+	fhandle = open(filename, O_RDONLY);
+
+	if(fhandle == -1){
+		exit(1);
+	}
+
+	free(filename);
+
+	int sum;
+	sum = 0;
+
+	pid_t *pids;
+	pids = malloc(sizeof(pid_t) * numProcesses);
+
+	int i;
+	for(i = 0; i < numProcesses; i++){
+		pids[i] = fork();
+
+		if(pids[i] == 0){
+			sum += processing_function(fhandle);		
+			return sum;
+		}
+	}
+
+	int status;
+
+	for(i = 0; i < numProcesses; i++){
+		wait(&status);			
+		sum += WEXITSTATUS(status);
+	}
+
+	printf("Sum: %i\n", sum);	
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tSum.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+#include "functions.h"
+
+static sem_t lock;
+
+void *thread_function(void *params){
+	int buffer[1];
+
+	while(read(*((int**)params)[0], buffer, sizeof(int)) != 0){
+		sem_wait(&lock);
+
+		*((int**)params)[1] += buffer[0];
+
+		sem_post(&lock);
+	}	
+
+	pthread_exit(0);
+}
+
+int main(int argc, char* argv[]){
+	
+	if(argc != 3){
+		exit(1);
+	}
+
+	char* 	filename; 
+	char* 	numThreadsStr;
+	int		numThreads;
+
+	filename = stringCopy(argv[1]);
+
+	numThreadsStr = stringCopy(argv[2]);
+	numThreads = atoi(numThreadsStr);
+	free(numThreadsStr);
+
+	int	fhandle;
+
+	fhandle = open(filename, O_RDONLY);
+
+	if(fhandle == -1){
+		exit(1);
+	}
+
+	free(filename);
+
+	int sum;
+	sum = 0;
+
+	pthread_t *thread_ids;
+	thread_ids = malloc(sizeof(pthread_t) * numThreads);
+
+	void** params;
+	params = malloc(sizeof(void*) * 2);
+	params[0] = &fhandle;
+	params[1] = &sum;
+
+	sem_init(&lock, 0, 1);
+
+	int success;
+	int i;
+	for(i = 0; i < numThreads; i++){
+		success = pthread_create(&thread_ids[i], NULL, &thread_function, params);		
+		if(success != 0){
+			exit(1);
+		}
+	}
+	
+	for(i = 0; i < numThreads; i++){
+		pthread_join(thread_ids[i], NULL);			
+	}
+
+	printf("Sum: %i\n", sum);
+
+	return 0;
+}
+
